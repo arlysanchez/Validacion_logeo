e985db3ee9f1e6fea6fe50ca4ab453f1dcbf6eb5
arlysanchez <jhansistema@gmail.com>
2015-05-25 19:17:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jhan/validacion_logeo/OnLoginListener.java
@@ -0,0 +1,8 @@
+package com.example.jhan.validacion_logeo;
+
+/**
+ * Created by JHAN  ARLY on 24/05/2015.
+ */
+public interface OnLoginListener {
+    void onLogin(String usuario, String password);
+}
